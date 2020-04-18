9bad1d6fd8b29a697f71725be2a65ac88c4fbc08
Agata Zurek <agata.w.zurek@gmail.com>
2020-04-17 19:18:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/**
+ * Array of objects each containg a Star Trek quote
+ * quote - a string containing the text of a quote
+ * source - a string containing the creator of the quote
+ * show - the Star Trek show the quote is from
+ * episode - the name of the Star Trek episode the quote is from
+ */
+const starTrekQuotes = [
+	{
+		quote   : 'It is possible to commit no mistakes and still lose. That is not weakness, that is life.',
+		source  : 'Captain Jean-Luc Picard',
+		show    : 'Star Trek: The Next Generation',
+		episode : 'Peak Performance'
+	},
+	{
+		quote  : 'You can use logic to justify almost anything. That’s its power. And its flaw.',
+		source : 'Captain Kathryn Janeway',
+    show   : 'Star Trek: Voyager',
+    episode: 'Prime Factors'
+	},
+	{
+		quote  : 'Curious, how often you humans manage to obtain that which you do not want.',
+		source : 'Spock',
+    show   : 'Star Trek: The Original Series',
+    episode: 'Errand of Mercy'
+	},
+	{
+		quote  : 'There are four lights!',
+		source : 'Captain Jean-Luc Picard',
+    show   : 'Star Trek: The Next Generation',
+    episode: 'Chain of Command'
+	}
+];
